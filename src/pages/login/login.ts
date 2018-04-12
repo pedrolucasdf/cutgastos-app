f49ba838a9c0ecbd548d6d967faf3f63bf4e6fce
Cristóvão Olegário <cristovaoolegario@gmail.com>
2018-04-12 00:51:55 -0300

    Criação da página de login.

---
+++ src/pages/login/login.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  onClickEntrar(){
+    //Chama o provider que autentica o usuário
+  }
+
+  onClickNovaConta(){
+    //Redireciona para a tela de cadastro.
+  }
+
+}
