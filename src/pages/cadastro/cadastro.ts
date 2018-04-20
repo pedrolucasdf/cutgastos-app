708cb8c32837232f7320c2b5e14fefbb73bcfee5
Cristóvão Olegário <cristovaoolegario@gmail.com>
2018-04-20 11:32:14 -0300

    Criação do data biding e a chamada ao provider de criação de conta.

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,8 @@
+import { JsonReturn } from './../../models/jsonReturn'; 
+import { SignInServiceProvider } from './../../providers/sign-in-service/sign-in-service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validator, Validators} from "@angular/forms";
 
 /**
  * Generated class for the CadastroPage page.
@@ -15,11 +18,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CadastroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  cadastroForm: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public singIn: SignInServiceProvider
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CadastroPage');
+    this.cadastroForm = this.formBuilder.group({
+      cpf : this.formBuilder.control("", [Validators.required]),
+      nome : this.formBuilder.control("", [Validators.required]),
+      endereco : this.formBuilder.control("", [Validators.required]),
+      email : this.formBuilder.control("", [Validators.required]),
+      senha : this.formBuilder.control("", [Validators.required]),
+      telefone : this.formBuilder.control("", [Validators.required]),
+      nascimento : this.formBuilder.control("", [Validators.required]),
+      sexo : this.formBuilder.control("", [Validators.required])
+    }) 
+  }
+
+  onClickCadastro(){
+    this.singIn.createAccount(this.cadastroForm.value)
+    .subscribe((response: JsonReturn)=> {
+      if(response.status === "SUCESSO"){
+        //Cadastro realizado com sucesso
+      }
+      else{
+        //Cadastro não pode ser realizado
+      }
+    })
   }
 
 }
