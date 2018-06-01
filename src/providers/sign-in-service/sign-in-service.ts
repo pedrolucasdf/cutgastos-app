a60f5c5288d91f32bcc1876936b76567d20ab6d2
Cristóvão Olegário <cristovaoolegario@gmail.com>
2018-06-01 00:06:57 -0300

    Atualização do provider de usuário.

--- src/providers/sign-in-service/sign-in-service.ts
+++ src/providers/sign-in-service/sign-in-service.ts
@@ -3,6 +3,7 @@ import { API_ENDPOINT } from './../../utils/constants';
 import { HttpClient } from '@angular/common/http'; 
 import { Injectable } from '@angular/core'; 
 import { Observable } from 'rxjs/Observable'; 
+import { Usuario } from '../../models/usuario';
 
 /*
   Generated class for the SignInServiceProvider provider.
@@ -17,8 +18,20 @@ export class SignInServiceProvider {
     console.log('Hello SignInServiceProvider Provider');
   }
 
-  createAccount(userData): Observable<JsonReturn>{ 
-    return this.http.post<JsonReturn>(API_ENDPOINT+"user", userData) 
-} 
+  createAccount(userData: Usuario): Observable<JsonReturn>{ 
+    return this.http.post<JsonReturn>(API_ENDPOINT+"usuario", userData); 
+  } 
+
+  updateAccount(userData: Usuario): Observable<JsonReturn>{
+    return this.http.put<JsonReturn>(API_ENDPOINT+"usuario?id="+userData.id, userData);
+  }
+
+  readAccount(userData: Usuario): Observable<JsonReturn>{
+    return this.http.get<JsonReturn>(API_ENDPOINT+"usuario?id="+userData.id);
+  }
+
+  deleteAccount(userData: Usuario): Observable<JsonReturn>{
+    return this.http.delete<JsonReturn>(API_ENDPOINT+"usuario?id="+userData.id);
+  }
 
 }
