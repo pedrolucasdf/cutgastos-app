42285a551438980c74e527e26b18823d03bfcda6
Franco Birro <fbirro@br.ibm.com>
2018-06-07 20:28:46 -0300

    Filtros nos gráficos e mudanzas na tela sobre

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,16 +21,20 @@ export class HomePage {
   @ViewChild('doughnutCanvas') doughnutCanvas;
   @ViewChild('lineCanvas') lineCanvas;
   @ViewChild('pieCanvas') pieCanvas;
+  @ViewChild('bartest') bartest;
 
   barChart: any;
   doughnutChart: any;
   lineChart: any;
   pieChart: any;
+  barTeste: any;
+
+  //qty: any;
 
   constructor(public navCtrl: NavController) {
 
   }
-
+  
   ionViewDidLoad() {
 
       this.barChart = new Chart(this.barCanvas.nativeElement, {
@@ -185,6 +189,56 @@ export class HomePage {
     
 
       });
+      this.barChart = new Chart(this.barCanvas.nativeElement, {
 
+        type: 'bar',
+        data: {
+            labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+            "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+            datasets: [{
+                label: 'kW',
+                data: [12, 19, 3, 5, 2, 3, 12, 19, 13, 15, 12, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+
+    });
   }
 }
