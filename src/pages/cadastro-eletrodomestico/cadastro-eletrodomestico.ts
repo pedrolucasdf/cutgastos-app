a7599f4c94d5f6315629ae848452e4ced1457cc1
Cristóvão Olegário <cristovaoolegario@gmail.com>
2018-06-07 20:32:27 -0300

    Correções no binding de cadastro de eletrodomésticos.

--- src/pages/cadastro-eletrodomestico/cadastro-eletrodomestico.ts
+++ src/pages/cadastro-eletrodomestico/cadastro-eletrodomestico.ts
@@ -36,8 +36,8 @@ export class CadastroEletrodomesticoPage {
         nome: this.formBuilder.control("", [Validators.required]),
         descricao: this.formBuilder.control("", [Validators.required]),
         quantidade: this.formBuilder.control("", [Validators.required]),
-        potenciaemuso: this.formBuilder.control("", [Validators.required]),
-        potenciaemstandby: this.formBuilder.control("", [Validators.required]),
+        potencia_uso: this.formBuilder.control("", [Validators.required]),
+        potencia_standby: this.formBuilder.control("", [Validators.required]),
         dias: this.formBuilder.control("", [Validators.required]),
         horas: this.formBuilder.control("", [Validators.required])
       })
@@ -47,13 +47,13 @@ export class CadastroEletrodomesticoPage {
       this.btnTexto = "Salvar";
       this.eletrodomestico = this.navParams.data;
       this.cadastroEletrodomesticoForm = this.formBuilder.group({
-        nome: this.formBuilder.control("", [Validators.required]),
-        descricao: this.formBuilder.control("", [Validators.required]),
-        quantidade: this.formBuilder.control("", [Validators.required]),
-        potenciaemuso: this.formBuilder.control("", [Validators.required]),
-        potenciaemstandby: this.formBuilder.control("", [Validators.required]),
-        dias: this.formBuilder.control("", [Validators.required]),
-        horas: this.formBuilder.control("", [Validators.required])
+        nome: this.formBuilder.control(this.eletrodomestico.nome, [Validators.required]),
+        descricao: this.formBuilder.control(this.eletrodomestico.descricao, [Validators.required]),
+        quantidade: this.formBuilder.control(this.eletrodomestico.quantidade, [Validators.required]),
+        potencia_uso: this.formBuilder.control(this.eletrodomestico.potencia_uso, [Validators.required]),
+        potencia_standby: this.formBuilder.control(this.eletrodomestico.potencia_standby, [Validators.required]),
+        dias: this.formBuilder.control(this.eletrodomestico.dias, [Validators.required]),
+        horas: this.formBuilder.control(this.eletrodomestico.horas, [Validators.required])
       })     
     }
   }
